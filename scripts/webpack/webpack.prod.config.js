677c96b716d29c79ed569b77cc4ce1319477ae9c
DragonYellow <786676631@qq.com>
2021-09-08 10:31:12 +0800

    test

--- scripts/webpack/webpack.prod.config.js
+++ scripts/webpack/webpack.prod.config.js
@@ -1,9 +1,18 @@
 // scripts/webpack/webpack-prod-config.js
 
-const { merge } = require('webpack-merge');
+const {
+  merge
+} = require('webpack-merge');
 
 const common = require('./webpack.common.config');
 
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 });
